cc2fac5aa0234e1f54ac32ed2f16c60415accbfa
AKKHARAWAT CHAYAPIWAT <aukwat@gmail.com>
2017-06-20 15:14:37 +0700

    Initialize Project 01

---
+++ Projects/01-WhoToFollow/WhoToFollow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WhoToFollow
+//
+//  Created by AKKHARAWAT CHAYAPIWAT on 6/20/17.
+//  Copyright © 2017 AKKHARAWAT CHAYAPIWAT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
